010f8d81b67380bae23debf8eda2b55246c737f6
Tommy Wang <tommy.wang2018@gmail.com>
2022-08-26 12:17:14 -0500

    Update Fix 2

--- ecommerce/src/main/java/com/hcl/commerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.hcl.commerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.hcl.commerce.dto.product.ProductAddDTO;
 import com.hcl.commerce.entity.Product;
 import com.hcl.commerce.service.product.ProductService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ProductController {
 	@Autowired
--- ecommerce/src/main/java/com/hcl/commerce/controller/UserController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hcl.commerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.hcl.commerce.dto.user.UserLoginDTO;
 import com.hcl.commerce.entity.User;
 import com.hcl.commerce.service.user.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UserController {
 	@Autowired
