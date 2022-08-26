4390237ff47dda080a7cfc3293cd2e7fec2d6b0a
Jordan <jordanlim324@gmail.com>
2022-08-26 11:35:21 -0500

    Backend

---
+++ ecommerce-frontend/src/app/common/address.ts
@@ -0,0 +1,8 @@
+export class Address {
+    street?: string;
+    secondary?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+    zipCode?: string;
+}
--- ecommerce/src/main/java/com/hcl/commerce/controller/InvoiceController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/InvoiceController.java
@@ -3,6 +3,7 @@ package com.hcl.commerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.hcl.commerce.dto.invoice.InvoiceReceiptDTO;
 import com.hcl.commerce.entity.Invoice;
 import com.hcl.commerce.service.invoice.InvoiceService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class InvoiceController {
 
