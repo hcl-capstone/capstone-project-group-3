98a1aec6c7a3b61eb8ec04bd5a6189dea7d87b54
Shanjida Khatun <shanjida.khatun@hcl.com>
2022-08-29 17:13:21 -0500

    View Orders

--- ecommerce-frontend/src/app/app.module.ts
+++ ecommerce-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +9,10 @@ import { ProductSearchComponent } from './components/product-search/product-sear
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-search/product-list/product-list.component';
+import { ProductService } from './services/product.service';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { InvoiceService } from './services/invoice.service';
 
 @NgModule({
   declarations: [
@@ -18,13 +22,16 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     ProductSearchComponent,
     RegisterUserComponent,
     UserDetailsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductListComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService, InvoiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
