eb126411c2450f93e17c898ba38d062484a9aba3
henryzshao <henryzshao@gmail.com>
2022-08-31 09:15:30 -0500

    Create UserRepositoryTests.java

---
+++ ecommerce/src/test/java/com/hcl/commerce/UserRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.hcl.commerce;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.hcl.commerce.entity.User;
+import com.hcl.commerce.repository.UserRepository;
+
+@DataJpaTest
+public class UserRepositoryTests {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Test
+	void saveUserTest() {
+		User user = new User();
+		user.setFirstName("First");
+		user.setLastName("Last");
+		user.setEmail("email@email.com");
+		user.setUsername("username");
+		user.setPassword("password");
+		
+		userRepo.save(user);
+		
+		Assertions.assertThat(user.getUserId()).isGreaterThan(0);
+		
+	}
+}
