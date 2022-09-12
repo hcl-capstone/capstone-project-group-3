92d9c7de579997c03919e15bb5c35d72065a6ff0
ash3022 <109552310+ash3022@users.noreply.github.com>
2022-09-12 13:24:05 -0500

    update

---
+++ Okta-angular.d.ts
@@ -0,0 +1,3 @@
+export * from './public-api';
+export { OktaHasAnyGroupDirective as ɵb } from './src/okta/has-any-group.directive';
+export { oktaAuthFactory as ɵa } from './src/okta/okta.module';
--- ecommerce-frontend/src/app/components/login/login.component.ts
+++ ecommerce-frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { AuthState, OktaAuth } from '@okta/okta-auth-js';
+import { filter, map, Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  title = 'okta-angular-quickstart';
+  public isAuthenticated$!: Observable<boolean>;
+  constructor(private _router: Router, private _oktaStateService: OktaAuthStateService, @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.isAuthenticated$ = this._oktaStateService.authState$.pipe(
+      filter((s: AuthState) => !!s),
+      map((s: AuthState) => s.isAuthenticated ?? false)
+    );
+  }
+  public async signIn() : Promise<void> {
+    await this._oktaAuth.signInWithRedirect().then(
+      _ => this._router.navigate(['/profile'])
+    );
   }
 
+  public async signOut(): Promise<void> {
+    await this._oktaAuth.signOut();
+  }
 }
