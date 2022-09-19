f83ad8c2ea3780fdedd97a5666c729c2dafbca3d
Tommy Wang <tommy.wang2018@gmail.com>
2022-09-19 11:30:02 -0500

    App Insights

--- ecommerce/src/main/java/com/hcl/commerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/hcl/commerce/EcommerceApplication.java
@@ -3,11 +3,15 @@ package com.hcl.commerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class EcommerceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+		log.info("application started");
 	}
 
 }
