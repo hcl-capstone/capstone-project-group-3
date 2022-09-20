b97f942ecc7773d0786721effc049fc7258b9e18
bbolem <bhuvantejb@gmail.com>
2022-09-20 14:38:24 -0500

    added crossorigin for testing functionalities

--- ecommerce/src/main/java/com/hcl/commerce/controller/UserSupportController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/UserSupportController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +18,7 @@ import com.hcl.commerce.service.user.UserSupportService;
 
 import lombok.extern.slf4j.Slf4j;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 @RestController
 public class UserSupportController {
