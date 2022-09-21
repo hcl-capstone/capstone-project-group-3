f0a0036ddacb19dc26eb8019034ef0412d920322
Tommy Wang <tommy.wang2018@gmail.com>
2022-09-21 15:48:56 -0500

    crossorigins

--- ecommerce/src/main/java/com/hcl/commerce/controller/InvoiceSupportController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/InvoiceSupportController.java
@@ -1,7 +1,7 @@
 package com.hcl.commerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +18,7 @@ import com.hcl.commerce.service.invoice.InvoiceSupportService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fruitilicious-frontend.azurewebsites.net")
 @RestController
 public class InvoiceSupportController {
 	
--- ecommerce/src/main/java/com/hcl/commerce/controller/ProductCategoryController.java
+++ ecommerce/src/main/java/com/hcl/commerce/controller/ProductCategoryController.java
@@ -3,7 +3,7 @@ package com.hcl.commerce.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +17,7 @@ import com.hcl.commerce.service.category.ProductCategoryService;
 
 import lombok.extern.slf4j.Slf4j;
 
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fruitilicious-frontend.azurewebsites.net")
 @Slf4j
 @RestController
 public class ProductCategoryController {
