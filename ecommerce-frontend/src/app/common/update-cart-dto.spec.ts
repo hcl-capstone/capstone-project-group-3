8fdcc8abc9c9a887005e50f300428287de12eb60
Your Name <samlucas383@gmail.com>
2022-09-21 14:59:20 -0500

    clean up

---
+++ ecommerce-frontend/src/app/common/update-cart-dto.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateCartDto } from './update-cart-dto';
+
+describe('UpdateCartDto', () => {
+  it('should create an instance', () => {
+    expect(new UpdateCartDto()).toBeTruthy();
+  });
+});
