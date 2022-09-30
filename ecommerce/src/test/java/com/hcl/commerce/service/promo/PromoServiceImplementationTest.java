dcf0a2ee154017c6335aebd9a926eddab452eaf3
bbolem <bhuvantejb@gmail.com>
2022-09-30 10:21:21 -0500

    changes to test cases

--- ecommerce/src/test/java/com/hcl/commerce/service/promo/PromoServiceImplementationTest.java
+++ ecommerce/src/test/java/com/hcl/commerce/service/promo/PromoServiceImplementationTest.java
@@ -1,6 +1,7 @@
 package com.hcl.commerce.service.promo;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
@@ -9,6 +10,8 @@ import static org.mockito.Mockito.when;
 import com.hcl.commerce.entity.Promo;
 import com.hcl.commerce.repository.PromoRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -24,6 +27,40 @@ class PromoServiceImplementationTest {
     private PromoRepository promoRepository;
     @Autowired
     private PromoServiceImplementation promoServiceImplementation;
+    /**
+     * Method under test: {@link PromoServiceImplementation#getPromo(String)}
+     */
+    @Test
+    void testGetPromo() {
+        Promo promo = new Promo();
+        promo.setDiscount(10.0d);
+        promo.setPromoCode("Promo Code");
+        Optional<Promo> ofResult = Optional.of(promo);
+        when(promoRepository.findByPromoCode((String) any())).thenReturn(ofResult);
+        assertSame(promo, promoServiceImplementation.getPromo("42"));
+        verify(promoRepository).findByPromoCode((String) any());
+    }
+    /**
+     * Method under test: {@link PromoServiceImplementation#getPromo(String)}
+     */
+    @Test
+    void testGetPromo2() {
+        when(promoRepository.findByPromoCode((String) any())).thenReturn(Optional.empty());
+        assertNull(promoServiceImplementation.getPromo("42"));
+        verify(promoRepository).findByPromoCode((String) any());
+    }
+    /**
+     * Method under test: {@link PromoServiceImplementation#getAllPromo()}
+     */
+    @Test
+    void testGetAllPromo() {
+        ArrayList<Promo> promoList = new ArrayList<>();
+        when(promoRepository.findAll()).thenReturn(promoList);
+        List<Promo> actualAllPromo = promoServiceImplementation.getAllPromo();
+        assertSame(promoList, actualAllPromo);
+        assertTrue(actualAllPromo.isEmpty());
+        verify(promoRepository).findAll();
+    }
     /**
      * Method under test: {@link PromoServiceImplementation#deletePromo(String)}
      */
